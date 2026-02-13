aed540ac9da6907f133a572d4f70e0f236161668
Alex Bogias <omikr0n@hotmail.com>
2026-02-13 21:59:44 +0200

    Add missing groupingIdentifier to GenericPass

--- src/Apple/Passes/GenericPass.php
+++ src/Apple/Passes/GenericPass.php
@@ -6,6 +6,19 @@ use Chiiya\Passes\Common\ArrayHelper;
 
 class GenericPass extends Pass
 {
+    public function __construct(
+        /**
+         * Optional.
+         * Identifier used to group related passes. If a grouping identifier is specified, passes with the same style,
+         * pass type identifier, and grouping identifier are displayed as a group. Otherwise, passes are grouped
+         * automatically.
+         */
+        public ?string $groupingIdentifier = null,
+        ...$args,
+    ) {
+        parent::__construct(...$args);
+    }
+
     public function encode(): array
     {
         $array = parent::encode();
